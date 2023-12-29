f46fd81f985b8032b000136c27c7bb9efbbb55c2
xoffproj3team1 <xoffgeneric@pm.me>
2023-12-29 11:40:30 -0800

    Generation of json file with x-wind info

--- Resources/airport_DB_queries.sql
+++ Resources/airport_DB_queries.sql
@@ -115,3 +115,16 @@ FROM apt_rwy
 JOIN apt_base ON apt_base.site_no = apt_rwy.site_no
 LEFT JOIN metar ON metar.station_id = apt_base.icao_id
 WHERE facility_use_code='PU' AND arpt_status='O';
+
+
+
+SELECT arpt_id, icao_id, arpt_name, apt_rwy.rwy_id, apt_rwy.rwy_len, apt_rwy_end.rwy_end_id, apt_rwy_end.true_alignment, metar.wind_dir_degrees, metar.wind_speed_kt, metar.wind_gust_kt,
+	ROUND(metar.wind_speed_kt*sin(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "cross_wind",
+	ROUND(metar.wind_speed_kt*cos(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "head_wind",
+	ROUND(metar.wind_gust_kt*sin(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "gust_cross_wind",
+	ROUND(metar.wind_gust_kt*cos(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "gust_head_wind"	
+FROM apt_rwy
+JOIN apt_base ON apt_base.site_no = apt_rwy.site_no
+JOIN apt_rwy_end ON apt_base.site_no = apt_rwy_end.site_no AND apt_rwy.rwy_id = apt_rwy_end.rwy_id
+FULL JOIN metar ON (RIGHT(metar.station_id, LENGTH(metar.station_id) - 1)) = apt_base.arpt_id
+WHERE facility_use_code='PU' AND arpt_status='O' AND site_type_code='A' AND (@(lat_decimal - metar.latitude) <1) AND (@(long_decimal - metar.longitude) <1) AND metar.wind_dir_degrees != 'VRB'
