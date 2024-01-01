4fd7788a195dea6e464ef62ec7f52529b59da34d
xoffproj3team1 <xoffgeneric@pm.me>
2024-01-01 13:40:45 -0800

    test Flask

--- weather_download.py
+++ weather_download.py
@@ -30,27 +30,6 @@ metar_data_df = pd.read_csv(url_metar_gz, header=5, compression='gzip')
 metar_data_df.head()
 
 # %%
-# Convert the dataframe to a JSON format
-# metar_json = json.loads(json.dumps(list(metar_data_df.T.to_dict().values())))   # https://stackoverflow.com/questions/39257147/convert-pandas-dataframe-to-json-format answer #10 Amir.S
-# pprint(metar_json, sort_dicts=False)  # the pprint process is slow (10.5sec)
-
-# %%
-# pprint(metar_json, sort_dicts=False)
-
-# %%
-# Convert some columns to INT
-# cols=['wind_dir_degrees','wind_speed_kt','wind_gust_kt','cloud_base_ft_agl','cloud_base_ft_agl.2',\
-#     'cloud_base_ft_agl.3','vert_vis_ft','elevation_m']
-# metar_data_df[cols]=metar_data_df[cols].apply(pd.to_numeric, errors='coerce', downcast='integer', axis=1) # Does not appear to convert to INT, but to FLOAT64
-# metar_data_df[cols]=metar_data_df[cols].astype(int)   # Cannot convert NaN
-
-# %%
-# metar_data_df.columns
-
-# %%
-# metar_data_df.info()
-
-# %%
 # convert the dataframe to a dictionary then to a JSON string
 metar_string=json.dumps(list(metar_data_df.T.to_dict().values()))
 
@@ -60,13 +39,6 @@ output_path = os.path.join("", "metar_data.json")
 with open(output_path, "w") as file:
     # write the JSON string to the file
     file.write(metar_string.replace("NaN","null"))
-
-# file is automatically closed after the with block
-
-# %%
-# To save the dataframe as a csv file for future import to database (not needed anymore)
-# output_path2 = os.path.join("", "metar_data.csv")
-# metar_data_df.to_csv(output_path2, index=False)
 
 # file is automatically closed after the with block
 
@@ -104,74 +76,6 @@ json_populate_recordset(NULL::metar, %s);
 
 cursor.execute(query_sql, (data,))
 connection.commit()
-
-# %% [markdown]
-# __To download a new json file made of the joining of airport data and weather data__ (not used anymore: To be deleted)
-
-# %%
-# # download the output of a query across multiple tables with all the active public airport with or without weather information.
-# # Will be used to populate the makers that do not have METAR information.
-# # There is only one row per airport so only one runway is listed even if the airport as more.
-# # More parameters can be added to complete the info on the popup window.
-
-# load_dotenv()
-# db_url = os.environ.get("link_render")
-
-# # query="""
-# #     SELECT DISTINCT ON (arpt_id)
-# # 	arpt_id, icao_id, metar.station_id, arpt_name, apt_rwy.rwy_id, lat_decimal, long_decimal, metar.observation_time, metar.wind_speed_kt, metar.flight_category, metar.raw_text
-# #     FROM apt_rwy
-# #     JOIN apt_base ON apt_base.site_no = apt_rwy.site_no
-# #     FULL JOIN metar ON metar.station_id = apt_base.icao_id
-# #     WHERE facility_use_code='PU' AND site_type_code='A' AND arpt_status='O';
-# #     """
-
-# query="""
-# SELECT DISTINCT ON (arpt_id)
-#     arpt_id, icao_id, metar.station_id, arpt_name, apt_rwy.rwy_id, lat_decimal, metar.latitude, long_decimal, metar.longitude, metar.observation_time, metar.wind_speed_kt, metar.flight_category, metar.raw_text, elev, metar.visibility_statute_mi, metar.cloud_base_ft_agl
-# FROM apt_rwy
-# JOIN apt_base ON apt_base.site_no = apt_rwy.site_no
-# FULL JOIN metar ON (RIGHT(metar.station_id, LENGTH(metar.station_id) - 1)) = apt_base.arpt_id
-# WHERE facility_use_code='PU' AND site_type_code='A' AND arpt_status='O' AND
-# CASE
-# 	WHEN metar.station_id IS NOT NULL
-# 	THEN (@(lat_decimal - metar.latitude) <1) AND (@(long_decimal - metar.longitude) <1) AND site_type_code='A'
-	
-# 	WHEN metar.station_id IS NULL
-# 	THEN site_type_code='A'
-# END
-# """
-
-# engine=create_engine(db_url)
-# with engine.begin() as conn:
-#     results=conn.execute(
-#         text(query)
-#     )
-
-# arpt_weather_query_df = pd.DataFrame(results)
-
-# # Save the df as a CSV file. Might not be needed if we only use JSON    TO BE REMOVED?
-# # arpt_weather_query_df.to_csv (r'airport_weather_data.csv', index = False) # place 'r' before the path name
-
-
-# # convert the dataframe to a dictionary then to a JSON string
-# arpt_weather_string=json.dumps(list(arpt_weather_query_df.T.to_dict().values()))
-
-# # open the file in write mode
-# output_path = os.path.join("", "airport_weather_data.json")
-# with open(output_path, "w") as file:
-#     # write the JSON string to the file
-#     file.write(arpt_weather_string.replace("NaN","null"))
-
-# # file is automatically closed after the with block
-
-
-
-# %%
-# arpt_weather_query_df.head()
-
-# %%
-# pprint(arpt_weather_string)
 
 # %% [markdown]
 # __To retrieve TAF data__
@@ -257,64 +161,6 @@ with open(output_path, "w") as file:
 # file is automatically closed after the with block
 
 # %% [markdown]
-# __To download headwing and crosswind informations for all runways__ (Not used anymore: to be deleted)
-
-# %%
-# # download the output of a query across multiple tables with the wind information relative to all runways.
-# # Will be used to update the makers for all airports.
-# # There are multiple rows per airport (one per runway).
-# # More parameters can be added to complete the info on the popup window.
-
-# load_dotenv()
-# db_url = os.environ.get("link_render")
-
-
-# query="""
-# SELECT arpt_id, icao_id, arpt_name, apt_rwy.rwy_id, apt_rwy.rwy_len, apt_rwy_end.rwy_end_id, apt_rwy_end.true_alignment, metar.wind_dir_degrees, metar.wind_speed_kt, metar.wind_gust_kt,
-# ROUND(metar.wind_speed_kt*sin(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "cross_wind",
-# ROUND(metar.wind_speed_kt*cos(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "head_wind",
-# ROUND(metar.wind_gust_kt*sin(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "gust_cross_wind",
-# ROUND(metar.wind_gust_kt*cos(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "gust_head_wind"	
-# FROM apt_rwy
-# JOIN apt_base ON apt_base.site_no = apt_rwy.site_no
-# JOIN apt_rwy_end ON apt_base.site_no = apt_rwy_end.site_no AND apt_rwy.rwy_id = apt_rwy_end.rwy_id
-# FULL JOIN metar ON (RIGHT(metar.station_id, LENGTH(metar.station_id) - 1)) = apt_base.arpt_id
-# WHERE facility_use_code='PU' AND arpt_status='O' AND site_type_code='A' AND (@(lat_decimal - metar.latitude) <1) AND (@(long_decimal - metar.longitude) <1) AND metar.wind_dir_degrees != 'VRB'
-# """
-
-# engine=create_engine(db_url)
-# with engine.begin() as conn:
-#     results=conn.execute(
-#         text(query)
-#     )
-
-# rwy_wind_query_df = pd.DataFrame(results)
-
-
-# # convert the dataframe to a dictionary then to a JSON string
-# rwy_wind_string=json.dumps(list(rwy_wind_query_df.T.to_dict().values()))
-
-# # open the file in write mode
-# output_path = os.path.join("", "rwy_wind_data.json")
-# with open(output_path, "w") as file:
-#     # write the JSON string to the file
-#     file.write(rwy_wind_string.replace("NaN","null"))
-
-# # file is automatically closed after the with block
-
-# %%
-# rwy_wind_query_df.head()
-
-# %%
-# len(rwy_wind_query_df)
-
-# %%
-# rwy_wind_query_df
-
-# %%
-
-
-# %% [markdown]
 # __Retrieve all data to position the circles for all airports and create the popup text__
 
 # %%
@@ -346,14 +192,11 @@ CASE
 	THEN site_type_code='A'
 END;
 
--- SELECT * FROM all_rwy_xwind_view;
-
-
 
 -- Query to get wind information for each runway as a VIEW
 DROP VIEW IF EXISTS all_rwy_xwind_view;
 CREATE VIEW all_rwy_xwind_view AS
-SELECT arpt_id, icao_id, arpt_name, apt_base.tpa, apt_rwy.rwy_id, apt_rwy.rwy_len, apt_rwy_end.rwy_end_id, apt_rwy_end.true_alignment, apt_rwy_end.right_hand_traffic_pat_flag, metar.wind_dir_degrees, metar.wind_speed_kt, metar.wind_gust_kt,
+SELECT arpt_id, icao_id, arpt_name, apt_base.tpa, apt_rwy.rwy_id, apt_rwy.rwy_len, apt_rwy.rwy_width, apt_rwy_end.rwy_end_id, apt_rwy_end.true_alignment, apt_rwy_end.right_hand_traffic_pat_flag, metar.wind_dir_degrees, metar.wind_speed_kt, metar.wind_gust_kt,
 	ROUND(metar.wind_speed_kt*sin(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "cross_wind",
 	ROUND(metar.wind_speed_kt*cos(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "head_wind",
 	ROUND(metar.wind_gust_kt*sin(radians(apt_rwy_end.true_alignment - (metar.wind_dir_degrees :: INTEGER)))) AS "gust_cross_wind",
@@ -375,7 +218,7 @@ END;
 
 -- Merge the two VIEWS
 SELECT 	all_circles_view.arpt_id, all_circles_view.icao_id, all_circles_view.station_id, all_circles_view.arpt_name, all_circles_view.lat_decimal, all_circles_view.long_decimal, all_circles_view.elev, all_rwy_xwind_view.tpa,
-		all_circles_view.rwy_id, all_rwy_xwind_view.rwy_end_id, all_rwy_xwind_view.rwy_len, all_rwy_xwind_view.right_hand_traffic_pat_flag,
+		all_circles_view.rwy_id, all_rwy_xwind_view.rwy_end_id, all_rwy_xwind_view.rwy_len, all_rwy_xwind_view.rwy_width, all_rwy_xwind_view.right_hand_traffic_pat_flag,
 		all_rwy_xwind_view.cross_wind, all_rwy_xwind_view.head_wind, all_rwy_xwind_view.gust_cross_wind, all_rwy_xwind_view.gust_head_wind,		
 		all_rwy_xwind_view.true_alignment,
 		all_rwy_xwind_view.wind_dir_degrees, all_rwy_xwind_view.wind_speed_kt, all_rwy_xwind_view.wind_gust_kt,
@@ -397,29 +240,18 @@ with engine.begin() as conn:
 airport_info_query_raw_df = pd.DataFrame(results)
 
 
-# # convert the dataframe to a dictionary then to a JSON string		# The section below will be moved to be used on the consolidated DF.
-# airport_info_string=json.dumps(list(airport_info_query_raw_df.T.to_dict().values()))
-
-# # open the file in write mode
-# output_path = os.path.join("", "airport_info_data.json")
-# with open(output_path, "w") as file:
-#     # write the JSON string to the file
-#     file.write(rwy_wind_string.replace("NaN","null"))
-
-# # file is automatically closed after the with block
+# %%
+# airport_info_query_raw_df.head()
 
 # %%
-airport_info_query_raw_df.head()
-
-# %%
-# airport_info_query_raw_df.columns
+airport_info_query_raw_df.columns
 
 # %%
 # Consolidation of the database with only one row per airport regardless of the number of runways
 
 airport_info_query_df=pd.DataFrame(columns=['arpt_id', 'icao_id', 'station_id', 'arpt_name', 'lat_decimal',
        'long_decimal', 'elev', 'tpa', 'rwy_id', 'rwy_end_id', 'rwy_len',
-       'right_hand_traffic_pat_flag', 'cross_wind', 'head_wind',
+       'rwy_width', 'right_hand_traffic_pat_flag', 'cross_wind', 'head_wind',
        'gust_cross_wind', 'gust_head_wind', 'true_alignment',
        'wind_dir_degrees', 'wind_speed_kt', 'wind_gust_kt', 'flight_category',
        'visibility_statute_mi', 'cloud_base_ft_agl', 'observation_time',
@@ -462,9 +294,11 @@ for i in range (a):
         popup_text=popup_text+'<br>'+"""<div class="rwy_id"> Runway """+airport_info_query_raw_df.iloc[i]["rwy_id"]+' </div>'
         if pd.isna(airport_info_query_raw_df.iloc[i]["rwy_len"])==False:
             popup_text=popup_text+'<br>'+"""<div class="rwy_len"> Runway length: """+str(int(airport_info_query_raw_df.iloc[i]["rwy_len"]))+' (ft)</div>'
+        if pd.isna(airport_info_query_raw_df.iloc[i]["rwy_width"])==False:
+            popup_text=popup_text+'<br>'+"""<div class="rwy_width"> Runway width: """+str(int(airport_info_query_raw_df.iloc[i]["rwy_width"]))+' (ft)</div>'
         first_rwy=False
 
-    if pd.isna(airport_info_query_raw_df.iloc[i]["raw_text"])==False:
+    if (pd.isna(airport_info_query_raw_df.iloc[i]["raw_text"])==False & pd.isna(airport_info_query_raw_df.iloc[i]["true_alignment"])==False) | (airport_info_query_raw_df.iloc[i]["right_hand_traffic_pat_flag"]=='Y'):
         if pd.isna(airport_info_query_raw_df.iloc[i]["rwy_end_id"])==False:
             popup_text=popup_text+'<br>'+"""<div class="rwy_end_id"> """+airport_info_query_raw_df.iloc[i]["rwy_end_id"]+' :</div>'
             if airport_info_query_raw_df.iloc[i]["right_hand_traffic_pat_flag"]=='Y':
@@ -493,8 +327,6 @@ for i in range (a):
                 popup_text=popup_text+'<br> <div class="raw_text">No weather information</div>'
 
             airport_info_query_df.at[k,"popup_text"]=popup_text
-            # print(f"k={k}, i={i}")
-            # print(popup_text)
             first_row=True
             first_rwy=True
         elif airport_info_query_raw_df.iloc[i+1]["rwy_id"]!=airport_info_query_raw_df.iloc[i]["rwy_id"]:
@@ -518,10 +350,13 @@ with open(output_path, "w") as file:
 # file is automatically closed after the with block
         
 
-
+# %%
 # airport_info_query_df
 
+# %%
+# airport_info_query_df[airport_info_query_df['arpt_id']=='BLU']
 
-
+# %% [markdown]
+# 
 
 
